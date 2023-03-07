82ad542c02c90ae6f4037c9ae4145cc966902873
hades <hadesify@yandex.ru>
2023-03-07 18:11:58 +0300

    finish Login.java

--- app/src/main/java/com/example/android_firebase_auth_java/Login.java
+++ app/src/main/java/com/example/android_firebase_auth_java/Login.java
@@ -1,14 +1,90 @@
 package com.example.android_firebase_auth_java;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    EditText email, password;
+    Button buttonLogin;
+    ProgressBar progressBar;
+    TextView textViewNotRegistered;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        progressBar = findViewById(R.id.progressBar);
+        textViewNotRegistered = findViewById(R.id.notRegistered);
+
+        buttonLogin.setOnClickListener(view -> {
+            String userEmail, userPassword;
+            userEmail = email.getText().toString();
+            userPassword = password.getText().toString();
+
+            progressBar.setVisibility(View.VISIBLE);
+
+            if (TextUtils.isEmpty(userEmail)) {
+                Toast.makeText(Login.this, "Enter email", Toast.LENGTH_SHORT).show();
+            }
+
+            if (TextUtils.isEmpty(userPassword)) {
+                Toast.makeText(Login.this, "Enter password", Toast.LENGTH_SHORT).show();
+            }
+
+            mAuth.signInWithEmailAndPassword(userEmail, userPassword)
+                    .addOnCompleteListener(task -> {
+                        progressBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Login.this, "Authentication succeed.",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        });
+
+        textViewNotRegistered.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), Registration.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
