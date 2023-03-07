65ae3df866e1babb7e1bab4ead20cfd058c2de6c
hades <hadesify@yandex.ru>
2023-03-07 16:25:21 +0300

    code refactoring

--- app/src/main/java/com/example/android_firebase_auth_java/Registration.java
+++ app/src/main/java/com/example/android_firebase_auth_java/Registration.java
@@ -1,8 +1,5 @@
 package com.example.android_firebase_auth_java;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -13,9 +10,8 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Registration extends AppCompatActivity {
@@ -38,47 +34,38 @@ public class Registration extends AppCompatActivity {
         progressBar = findViewById(R.id.progressBar);
         textViewAlreadyRegistered = findViewById(R.id.alreadyRegistered);
 
-        buttonRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String userEmail, userPassword;
-                userEmail = String.valueOf(email.getText());
-                userPassword = String.valueOf(password.getText());
+        buttonRegister.setOnClickListener(view -> {
+            String userEmail, userPassword;
+            userEmail = String.valueOf(email.getText());
+            userPassword = String.valueOf(password.getText());
 
-                progressBar.setVisibility(View.VISIBLE);
+            progressBar.setVisibility(View.VISIBLE);
 
-                if (TextUtils.isEmpty(userEmail)) {
-                    Toast.makeText(Registration.this, "Enter email", Toast.LENGTH_SHORT).show();
-                }
-
-                if (TextUtils.isEmpty(userPassword)) {
-                    Toast.makeText(Registration.this, "Enter password", Toast.LENGTH_SHORT).show();
-                }
-
-                mAuth.createUserWithEmailAndPassword(userEmail, userEmail)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                progressBar.setVisibility(View.GONE);
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(Registration.this, "Authentication succeed.",
-                                            Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Toast.makeText(Registration.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
+            if (TextUtils.isEmpty(userEmail)) {
+                Toast.makeText(Registration.this, "Enter email", Toast.LENGTH_SHORT).show();
             }
+
+            if (TextUtils.isEmpty(userPassword)) {
+                Toast.makeText(Registration.this, "Enter password", Toast.LENGTH_SHORT).show();
+            }
+
+            mAuth.createUserWithEmailAndPassword(userEmail, userEmail)
+                    .addOnCompleteListener(task -> {
+                        progressBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Registration.this, "Authentication succeed.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(Registration.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
         });
 
-        textViewAlreadyRegistered.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
-                finish();
-            }
+        textViewAlreadyRegistered.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
