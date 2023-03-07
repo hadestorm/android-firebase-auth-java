1d2b8fb654d4b1f961d9c4b4b551bb08cb45730b
hades <hadesify@yandex.ru>
2023-03-07 18:11:50 +0300

    finish MainActivity.java

--- app/src/main/java/com/example/android_firebase_auth_java/MainActivity.java
+++ app/src/main/java/com/example/android_firebase_auth_java/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.android_firebase_auth_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    FirebaseUser user;
+    Button logoutButton;
+    TextView userDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        logoutButton = findViewById(R.id.logoutButton);
+        userDetails = findViewById(R.id.userEmailData);
+        user = auth.getCurrentUser();
+
+        if (user == null) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        } else {
+            userDetails.setText(user.getEmail());
+        }
+
+        logoutButton.setOnClickListener(view -> {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
