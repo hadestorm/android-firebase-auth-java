f559bf9ac9d9f42397083deb0c7856015dc2e49b
hades <hadesify@yandex.ru>
2023-03-07 16:24:30 +0300

    add registration

--- app/src/main/java/com/example/android_firebase_auth_java/Registration.java
+++ app/src/main/java/com/example/android_firebase_auth_java/Registration.java
@@ -1,44 +1,83 @@
 package com.example.android_firebase_auth_java;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Registration extends AppCompatActivity {
-
-    EditText editEmailField, editPasswordField;
-    Button registerButton;
+    FirebaseAuth mAuth;
+    EditText email, password;
+    Button buttonRegister;
+    ProgressBar progressBar;
+    TextView textViewAlreadyRegistered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        editEmailField = findViewById(R.id.email);
-        editPasswordField = findViewById(R.id.password);
-        registerButton = findViewById(R.id.buttonRegister);
+        mAuth = FirebaseAuth.getInstance();
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        buttonRegister = findViewById(R.id.buttonRegister);
+        progressBar = findViewById(R.id.progressBar);
+        textViewAlreadyRegistered = findViewById(R.id.alreadyRegistered);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String userEmail , userPassword;
-                userEmail = String.valueOf(editEmailField.getText());
-                userPassword = String.valueOf(editPasswordField.getText());
+                String userEmail, userPassword;
+                userEmail = String.valueOf(email.getText());
+                userPassword = String.valueOf(password.getText());
 
-                if(TextUtils.isEmpty(userEmail)) {
+                progressBar.setVisibility(View.VISIBLE);
+
+                if (TextUtils.isEmpty(userEmail)) {
                     Toast.makeText(Registration.this, "Enter email", Toast.LENGTH_SHORT).show();
                 }
 
-                if(TextUtils.isEmpty(userPassword)) {
+                if (TextUtils.isEmpty(userPassword)) {
                     Toast.makeText(Registration.this, "Enter password", Toast.LENGTH_SHORT).show();
                 }
+
+                mAuth.createUserWithEmailAndPassword(userEmail, userEmail)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Registration.this, "Authentication succeed.",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(Registration.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        textViewAlreadyRegistered.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
